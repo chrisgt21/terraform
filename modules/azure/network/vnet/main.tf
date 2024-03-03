1ae1ba790cfc17e0f4c0556edd0038514bc98d93
Christopher Tsatsis <>
2024-03-03 18:17:08 -0500

    edit dns_servers

--- modules/azure/network/vnet/main.tf
+++ modules/azure/network/vnet/main.tf
@@ -3,6 +3,11 @@ resource "azurerm_virtual_network" "vnet" {
     location                = var.location
     resource_group_name     = var.resource_group_name
     address_space           = var.address_space
-    dns_servers             = var.dns_servers
+    dynamic "dns_servers" {
+        for_each = var.dns_servers != null ? [var.dns_servers] : []
+        content {
+            dns_servers = dns_servers.value
+        }
+    }   
     
 }
